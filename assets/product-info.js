e13e0ce92e362b2d4e0494ec3d985cd3398d906a
HRB-StratNova <hrishabh@stratnova.ai>
2025-08-07 01:43:22 +0530

    code-format/ white spaces removed

--- assets/product-info.js
+++ assets/product-info.js
@@ -131,8 +131,6 @@ if (!customElements.get('product-info')) {
           .catch((error) => {
             if (error.name === 'AbortError') {
               console.log('Fetch aborted by user');
-            } else {
-              console.error(error);
             }
           });
       }
